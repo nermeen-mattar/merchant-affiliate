9e006f7d7577814831fb20c4c9179f06b5d405e6
Nermeen Mattar <nermeen.mattar@cr2.com>
2018-01-28 22:48:37 +0200

    Removing unnecessary comments.

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -4,15 +4,6 @@ import { LoginComponent } from './components/login/login.component';
 import { ResetComponent } from './components/reset/reset.component';
 import { RegisterComponent } from './components/register/register.component';
 
-/**
- * Paths will look to the end user as follow:
- * domainName/login -> loads the login components
- * domainName/login/reset -> loads the reset password component
- * Suggestion:
- * domainName/auth/login -> loads the login components
- * domainName/auth/reset -> loads the reset password component
- * domainName/auth/regester -> loads the regester (or move regester to the home module as it is not related to authentication)
- */
 const authRoutes: Routes = [
 
     {
@@ -32,7 +23,7 @@ const authRoutes: Routes = [
                 component: ResetComponent
             },
             {
-                path: 'register', // should we include it in another module?
+                path: 'register',
                 component: RegisterComponent
             }
         ]
