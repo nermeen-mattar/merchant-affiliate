91a54c5029b8454431074f662cb2da1f9470dbf7
Nermeen Mattar <n.mattar@posrocket.com>
2018-02-03 20:33:34 +0200

    Changing in auth guard to be support the following: 1- Preventing the user from visiting auth or home modules the user is authenticated. 2- If the user is not authenticated it prevents visiting account based pages. 3- Navigates to the correct module in case the user is manually changing the route and can activate is false.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,38 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 
 /* The default route is the team center module but if the user is not logged in auth gaurd will redirect the user to the home */
-const routes: Routes = [
-  {
+const routes: Routes = [{
     path: '',
-    loadChildren: './team-center/team-center.module#TeamCenterModule',
-    canActivate: [AuthGuard]
+    pathMatch: 'full',
+    redirectTo: 'home'
   },
   {
     path: 'home',
+    canActivate: [AuthGuard],
     loadChildren: './home/home.module#HomeModule'
   },
   {
     path: 'auth',
+    canActivate: [AuthGuard],
     loadChildren: './auth/auth.module#AuthModule',
   },
+  {
+    path: 'events',
+    canActivate: [AuthGuard],
+    loadChildren: './team-center/events/events.module#EventsModule'
+  },
+  {
+    path: 'members',
+    canActivate: [AuthGuard],
+    loadChildren: './team-center/members/members.module#MembersModule'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ];
 
 @NgModule({
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { baseUrl } from './../../../environments/environment.prod';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../auth/services/auth.service';
 
+/**
+ *
+ * @author Nermeen Mattar
+ * @export
+ * @class AuthGuard
+ * @implements {CanActivate}
+ * @description If the user is authenticated it prevents visiting auth or home module and if not authenticated prevents
+ *  visiting account based pages. Moreover, it navigates to the correct module in case the user is manually changing the
+ *  route and can activate is false.
+ */
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private router: Router,
@@ -10,13 +21,25 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable < boolean > | Promise < boolean > | boolean {
-      console.log(this.authService.isAuthenticated());
+      console.log('this.authService.isAuthenticated() ', this.authService.isAuthenticated());
     if (this.authService.isAuthenticated()) {
-      return true;
+      if (state.url.indexOf('home') > -1  || state.url.indexOf('auth') > -1 ) {
+        if (this.router.routerState.snapshot.url === '') {
+          this.router.navigateByUrl('events'); // when manually changing the route to a wrong route or to a non account based page.
+        }
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      if (state.url.indexOf('home') > -1  || state.url.indexOf('auth') > -1 ) {
+        return true;
+      } else {
+        if (this.router.routerState.snapshot.url === '') {
+          this.router.navigateByUrl('home'); // when manually changing the route to a wrong route or to an account based page.
+        }
+        return false;
+      }
     }
-    // not logged in so redirect to login page
-    // this.router.navigate(['/']); // login
-    return false;
-
   }
 }
