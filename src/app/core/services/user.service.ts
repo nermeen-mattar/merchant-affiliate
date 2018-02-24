55c0db97e2fee30c43b2d8c4e8e720d3ae819923
Nermeen Mattar <n.mattar@posrocket.com>
2018-02-24 09:29:53 +0200

    Refactoring/enhancing user service and adding new info.

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,34 +1,86 @@
 import { Injectable } from '@angular/core';
 
+import { TeamRoles } from './../../teams/models/team-roles.model';
+
 @Injectable()
 export class UserService {
+  private _username: string;
+  private _teamRoles: TeamRoles;
+  private _userType: string;
 
-  constructor() { }
+  constructor() {
+    this._username = JSON.parse(localStorage.getItem('username'));
+    this._teamRoles = JSON.parse(localStorage.getItem('teamRoles'));
+    this._userType = JSON.parse(localStorage.getItem('userType'));
+  }
 
   /**
    * @author Nermeen Mattar
-   * @description gets the username/email from the local storage
-   * @returns {string}
+   * @description returns the username/email for the logged in user
+   * @readonly
+   * @type {string}
    */
-  getUsername(): string {
-    return JSON.parse(localStorage.getItem('username'));
+  get username(): string {
+    return this._username;
   }
   /**
    * @author Nermeen Mattar
-   * @description sets the username/email to the local storage
-   * @param username
+   * @description sets the username/email in a private variable
+   * @param {username} string
    */
-  setUsername(username: string): void { // will set it locally too
+  set username(username: string) {
+    this._username = username;
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description returns the team roles for the logged in user
+   * @readonly
+   * @type {TeamRoles}
+   */
+  get teamRoles(): TeamRoles {
+    return this._teamRoles;
+  }
+  /**
+   * @author Nermeen Mattar
+   * @description sets the team roles in a private variable
+   * @param {teamRoles} TeamRoles
+   */
+  set teamRoles(teamRoles: TeamRoles) {
+    this._teamRoles = teamRoles;
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description returns the user type (ordinary user or admin) for the logged in user
+   * @readonly
+   * @type {string}
+   */
+  get userType(): string {
+    return this._userType;
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description sets the user type (ordinary user or admin) in a private variable
+   * @param {userType} string
+   */
+  set userType(userType: string) {
+    this._userType = userType;
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description stores the username, the team roles, and the user type ordinary/admin in the local storage and in private variables
+   * @param {string} username
+   * @param {TeamRoles} teamRoles
+   */
+  storeLoggedInUserInfo(username: string, teamRoles: TeamRoles, userType: string) {
+    this._username = username;
     localStorage.setItem('username', JSON.stringify(username));
-  }
-
-  /**
-   * @author Nermeen Mattar
-   * @description sets the team roles to the local storage
-   * @param username
-   */
-  setTeamRoles(teamRoles): void { // add type (team-role.model.ts)
+    this._teamRoles = teamRoles;
     localStorage.setItem('teamRoles', JSON.stringify(teamRoles));
+    this._userType = userType;
+    localStorage.setItem('userType', JSON.stringify(userType));
   }
-
 }
