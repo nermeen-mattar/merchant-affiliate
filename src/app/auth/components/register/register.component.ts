3d18c7a4a783ddbd83a3aecfa588a50c344bed5d
Nermeen Mattar <n.mattar@posrocket.com>
2018-04-11 11:36:22 +0300

    Proceeding work on register page changes (Supporting multi steps, and validating before going to next step).

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
 
 import { HttpRequestsService } from '../../../core/services/http-requests.service';
 import { AuthService } from '../../services/auth.service';
-import { ClientSideRegisterInfo } from '../../models/client-side-register-info.model';
+import { AdminRegisterInfo } from './../../models/admin-register-info.model';
+import { TeamRegisterInfo } from './../../models/team-register-info.model';
 
 @Component({
   selector: 'tc-register',
@@ -14,25 +16,42 @@ export class RegisterComponent implements OnInit {
   constructor(private authService: AuthService) {}
 
   ngOnInit() {}
+  selectedStepChanged(changeInfo: StepperSelectionEvent) {
+    if (changeInfo.previouslySelectedIndex === 0) {
+      this.checkIfAdminAlreadyExist(changeInfo.previouslySelectedStep.stepControl.value);
+    }
+  }
+
+  checkIfAdminAlreadyExist(firstStepValue) { // add typing
+   /* this.authService.isAdminExist({
+     email: firstStepValue.email
+    }).subscribe(
+      res => {
+      console.log(res);
+    }, err => { // not catching the error
+      console.log(err);
+    });*/
+  }
 
 /**
  * @author Nermeen Mattar
  * @description registering a new user, once registration is successful the new user will get logged in using credentials received from the
  * server
- * @param {ClientSideRegisterInfo} registerFormValue
+ * @param {TeamRegisterInfo} teamInfo
+ * @param {AdminRegisterInfo} adminInfo
  */
-register(registerFormValue: ClientSideRegisterInfo) {
+register(teamInfo: TeamRegisterInfo, adminInfo: AdminRegisterInfo) {
     this.authService.register({
-      teamname: registerFormValue.teamName,
-      teampassword: registerFormValue.teamPassword,
-      email: registerFormValue.email,
-      firstname: registerFormValue.firstName,
-      lastname: registerFormValue.lastName,
-      adminpassword: registerFormValue.adminPassword
+      teamname: teamInfo.teamName,
+      teampassword: teamInfo.teamPassword,
+      email: teamInfo.email,
+      firstname: adminInfo.firstName,
+      lastname: adminInfo.lastName,
+      adminpassword: adminInfo.adminPassword
     }).subscribe((res) => {
       this.authService.login({
-        username: registerFormValue.email,
-        password: registerFormValue.adminPassword
+        username: teamInfo.email,
+        password: adminInfo.adminPassword
       });
     });
   }
