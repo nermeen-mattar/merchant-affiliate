cdf57fd3f3641c6f12f2149714efadc2d095bfa9
Nermeen Mattar <n.mattar@posrocket.com>
2018-04-14 22:34:54 +0300

    Fixing pipeline issues.

--- src/app/core/services/admin.service.spec.ts
+++ src/app/core/services/admin.service.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AdminService } from './admin.service';
+import { HttpRequestsService } from './http-requests.service';
+import { HttpRequestsServiceMock } from '../../shared/mocks/services/http-requests.service.mock';
+
 
 describe('AdminService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AdminService]
+      providers: [{
+          provide: HttpRequestsService,
+          useClass: HttpRequestsServiceMock
+        },
+        AdminService
+      ]
     });
   });
 
