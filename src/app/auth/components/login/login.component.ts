9fe352ebbed03450919f64ff1f2c9e758c63dcd2
Nermeen Mattar <n.mattar@posrocket.com>
2018-05-03 13:14:36 +0300

    Supporting logging in using mail not exist and mail not confirmed cases.

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RegisterService } from './../../services/register.service';
 import { AuthService } from '../../services/auth.service';
 import { ClientSideLoginInfo } from '../../models/client-side-login-info.model';
 
@@ -10,25 +11,49 @@ import { ClientSideLoginInfo } from '../../models/client-side-login-info.model';
 })
 export class LoginComponent implements OnInit {
   displaySpinner = false;
-  constructor(private authService: AuthService) { }
+  displayMessage= "mailNotExist";
+  enteredEmail: string;
+  constructor(private authService: AuthService, private registerService: RegisterService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
- /**
-  * @author Nermeen Mattar
-  * @description logging in for an existing user
-  * @param {ClientSideLoginInfo} loginFormValue
-  */
- login(loginFormValue: ClientSideLoginInfo) {
-   this.displaySpinner = true;
-    this.authService.login({
-      username: loginFormValue.userEmail,
-      password: loginFormValue.userPassword
-    }).subscribe(res => {
+  /**
+   * @author Nermeen Mattar
+   * @description attempts to resend the activation mail.
+   */
+  resendConfirmationEmail() {
+    this.displaySpinner = true;
+    this.registerService.resendActivationMail(this.enteredEmail).subscribe(res => {
+      this.displaySpinner = false;
     }, err => {
       this.displaySpinner = false;
     });
   }
+
+  /**
+   * @author Nermeen Mattar
+   * @description attempts to login. In case of using a non-confirmed email a message will be displayed indicating that the user have to
+   * confirm the email first. A link to resend the mail confirmation link will also be displayed. In case of using an not existing email
+   * a message will be displayed indicating that the user does not exist along with a ling to navigate to the register page.
+   * @param {ClientSideLoginInfo} loginFormValue
+   */
+  login(loginFormValue: ClientSideLoginInfo) {
+    this.displaySpinner = true;
+    this.enteredEmail = loginFormValue.userEmail;
+    this.authService.login({
+      username: loginFormValue.userEmail,
+      password: loginFormValue.userPassword
+    }).subscribe(res => {
+      /* we can remove the following two lines as once successful login the user will get navigated. */
+      this.displaySpinner = false;
+      this.displayMessage = null;
+    }, err => {
+      this.displaySpinner = false;
+      if (err.error.message === 'error.mail.not.confirmed') {
+        this.displayMessage = 'mailNotActivated';
+      } else if (err.error.message === 'error.mail.not.exist') {
+        this.displayMessage = 'mailNotExist';
+      }
+    });
+  }
 }
- 
