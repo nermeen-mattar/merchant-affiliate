0e0b15fbfa60a96c1d8ca81c747bd4aa5a13d5d2
Nermeen Mattar <n.mattar@posrocket.com>
2018-06-16 20:16:52 +0300

    Adding remove admin role and remove member role functions and call it when a team gets deleted.

--- src/app/core/services/teams.service.ts
+++ src/app/core/services/teams.service.ts
@@ -1,13 +1,12 @@
+
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 import { TcTeamInfo } from './../../teams/models/tc-team-info.model';
 import { TcClientSideTeamRoles } from './../../teams/models/tc-client-side-team-roles.model';
 import { TcServerSideTeamRoles } from '../../teams/models/tc-server-side-team-roles.model';
-import { roles } from '../constants/roles.constants';
 import { HttpRequestsService } from './http-requests.service';
-import { map } from 'rxjs/operators';
-
 @Injectable()
 
 export class TeamsService {
@@ -20,30 +19,6 @@ export class TeamsService {
     this.teamRoles = JSON.parse(localStorage.getItem('teamRoles'));
   }
 
-  /**
-   * @author Nermeen Mattar
-   * @description returns the team roles for the logged in user
-   * @readonly
-   * @type {TcClientSideTeamRoles}
-   */
-  get teamRoles(): TcClientSideTeamRoles {
-    return this._teamRoles;
-  }
-
-  /**
-   * @author Nermeen Mattar
-   * @description sets the team roles in a private variable then it either sets them in the localstorage or remove them from the
-   *  localstorage
-   * @param {teamRoles} TcClientSideTeamRoles
-   */
-  set teamRoles(teamRoles: TcClientSideTeamRoles) {
-    this._teamRoles = teamRoles;
-    if (this.teamRoles) {
-      localStorage.setItem('teamRoles', JSON.stringify(teamRoles));
-    } else {
-      localStorage.removeItem('teamRoles');
-    }
-  }
   /**
    * @author Nermeen Mattar
    * @description prepares an array for teams list and an array for teams roles then calls a function to fill the two arrays. The function
@@ -60,34 +35,6 @@ export class TeamsService {
 
     this.teamRoles = teamRoles;
     this.userTeams = teamsList;
-  }
-
-  /**
-   * @author Nermeen Mattar
-   * @description check wether the user with the passed id has a member role
-   * @param {number} teamId
-   * @returns {boolean}
-   */
-  hasMemberRole(teamId?: number): boolean {
-    if (teamId) {
-      return this.teamRoles.teamMembers.includes(teamId);
-    } else {
-      return Boolean(this.teamRoles.teamMembers.length);
-    }
-  }
-
-  /**
-   * @author Nermeen Mattar
-   * @description check wether the user with the passed id has an admin role
-   * @param {number} teamId
-   * @returns {boolean}
-   */
-  hasAdminRole(teamId?: number): boolean {
-    if (teamId) {
-      return this.teamRoles.teamAdmins.includes(teamId);
-    } else {
-      return Boolean(this.teamRoles.teamAdmins.length);
-    }
   }
 
   /**
@@ -120,14 +67,31 @@ export class TeamsService {
     }
   }
 
+
   /**
    * @author Nermeen Mattar
-   * @description filters the teams list to get only the teams that the user is admin of and sets this value in the teamsTheUserIsAdminOf.
+   * @description returns the team roles for the logged in user
+   * @readonly
+   * @type {TcClientSideTeamRoles}
    */
-  getTeamsTheUserIsAdminOf() {
-    return this.userTeams.filter(team => this.teamRoles.teamAdmins.includes(team.teamId));
+  get teamRoles(): TcClientSideTeamRoles {
+    return this._teamRoles;
   }
 
+  /**
+   * @author Nermeen Mattar
+   * @description sets the team roles in a private variable then it either sets them in the localstorage or remove them from the
+   *  localstorage
+   * @param {teamRoles} TcClientSideTeamRoles
+   */
+  set teamRoles(teamRoles: TcClientSideTeamRoles) {
+    this._teamRoles = teamRoles;
+    if (this.teamRoles) {
+      localStorage.setItem('teamRoles', JSON.stringify(teamRoles));
+    } else {
+      localStorage.removeItem('teamRoles');
+    }
+  }
 
   /**
    * @author Nermeen Mattar
@@ -158,6 +122,36 @@ export class TeamsService {
 
   /**
    * @author Nermeen Mattar
+   * @description returns team the user has selected from the list of team he/she is admin/member of.
+   */
+  get selectedTeamId(): number {
+    return this._selectedTeamId;
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description sets the selected team in a private variable and in the localstorage based on the user selection from the list of teams
+   * he/she is admin/member of.
+   */
+  set selectedTeamId(selectedTeamId: number) {
+    this._selectedTeamId = selectedTeamId;
+    if (selectedTeamId) {
+      localStorage.setItem('selectedTeamId', JSON.stringify(selectedTeamId));
+    } else {
+      localStorage.removeItem('selectedTeamId');
+    }
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description filters the teams list to get only the teams that the user is admin of and sets this value in the teamsTheUserIsAdminOf.
+   */
+  getTeamsTheUserIsAdminOf() {
+    return this.userTeams.filter(team => this.teamRoles.teamAdmins.includes(team.teamId));
+  }
+
+  /**
+   * @author Nermeen Mattar
    * @description attemps to update the team name for the team with the passed teamId using the httpPut function from httpRequestsSevrice.
    * Upon successful request the team name for the modifier team will be changed in team roles.
    * @param {string} newTeamName
@@ -177,14 +171,62 @@ export class TeamsService {
 
   /**
    * @author Nermeen Mattar
-   * @description adds a new team role (if not already exist) to the user roles for the team with the passed teamId
+   * @description check wether the user with the passed id has a member role
+   * @param {number} teamId
+   * @returns {boolean}
+   */
+  hasMemberRole(teamId ? : number): boolean {
+    if (teamId) {
+      return this.teamRoles.teamMembers.includes(teamId);
+    } else {
+      return Boolean(this.teamRoles.teamMembers.length); /* may move this line to the user service */
+    }
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description check wether the user with the passed id has an admin role
+   * @param {number} teamId
+   * @returns {boolean}
+   */
+  hasAdminRole(teamId ? : number): boolean {
+    if (teamId) {
+      return this.teamRoles.teamAdmins.includes(teamId);
+    } else {
+      return Boolean(this.teamRoles.teamAdmins.length);
+    }
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description adds a member role (if not already exist) to the user roles for the team with the passed teamId
    * @param {number} teamId
    */
   addMemberRole(teamId: number) {
-    if (!this.teamRoles.teamMembers.includes(teamId)) {
+    if (!this.teamRoles.teamMembers.includes(teamId)) { /* may generalize this to be addRole(rolename) */
       this.teamRoles.teamMembers.push(teamId);
       localStorage.setItem('teamRoles', JSON.stringify(this.teamRoles));
     }
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description removes the member role from the user roles for the team with the passed teamId
+   * @param {number} teamId
+   */
+  removeMemberRole(teamId: number) {
+    this.teamRoles.teamMembers = this.teamRoles.teamMembers.filter(currTeamId => currTeamId !== teamId);
+    this.teamRoles = this.teamRoles;
+  }
+
+  /**
+   * @author Nermeen Mattar
+   * @description removes the admin role from the user roles for the team with the passed teamId
+   * @param {number} teamId
+   */
+  removeAdminRole(teamId: number) {
+    this.teamRoles.teamAdmins = this.teamRoles.teamAdmins.filter(currTeamId => currTeamId !== teamId);
+    this.teamRoles = this.teamRoles;
   }
 
   /**
@@ -197,34 +239,14 @@ export class TeamsService {
       success: 'TEAM.TEAM_DELETING_SUCCESS',
       failDefault: 'TEAM.TEAM_DELETING_ERROR'
     }).pipe(map(res => {
+      this.removeAdminRole(teamId);
+      this.removeMemberRole(teamId);
       this.userTeams = this.userTeams.filter(userTeam => userTeam.teamId !== teamId);
-      if (teamId === this.selectedTeamId && this.userTeams && this.userTeams.length) {
+      if (teamId === this.selectedTeamId && this.userTeams.length) {
         this.selectedTeamId = this.userTeams[0].teamId;
       }
       return res;
     }));
-  }
-
-  /**
-   * @author Nermeen Mattar
-   * @description returns team the user has selected from the list of team he/she is admin/member of.
-   */
-  get selectedTeamId(): number {
-    return this._selectedTeamId;
-  }
-
-  /**
-   * @author Nermeen Mattar
-   * @description sets the selected team in a private variable and in the localstorage based on the user selection from the list of teams
-   * he/she is admin/member of.
-   */
-  set selectedTeamId(selectedTeamId: number) {
-    this._selectedTeamId = selectedTeamId;
-    if (selectedTeamId) {
-      localStorage.setItem('selectedTeamId', JSON.stringify(selectedTeamId));
-    } else {
-      localStorage.removeItem('selectedTeamId');
-    }
   }
 
   /**
