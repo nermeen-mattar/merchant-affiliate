1f03ad3cd813f3437e75206dd0097c0a0c52bad8
Nermeen Mattar <n.mattar@posrocket.com>
2018-07-17 02:48:48 +0300

    Removing isAdminExist function.

--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -9,19 +9,6 @@ export class AdminService {
 
   constructor(private httpRequestService: HttpRequestsService, private userService: UserService, private teamsService: TeamsService) {}
 
-  /**
-   * @author Nermeen Mattar
-   * @description Uses the httpRequestsSevrice to send a post request to the backend to check whether the received email belongs to an
-   * already existed admin. Note that an object of type UserMessages is being sent with an empty fail property to disable defulat error.
-   * @param emailObj
-   */
-  isAdminExist(email: string): Observable < any > {
-    return this.httpRequestService.httpPost('teamadmins/check', {
-      email: email
-    }, {
-      fail: 'NO_ERROR_MESSAGE'
-    });
-  }
 
   /**
    * @author Nermeen Mattar
