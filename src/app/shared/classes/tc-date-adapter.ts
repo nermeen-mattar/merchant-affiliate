f42161fe78b444aa38e0136559e4af5e9d090b79
Nermeen Mattar <n.mattar@posrocket.com>
2018-11-09 02:11:51 +0200

    Changing Datepicker format

--- src/app/shared/classes/tc-date-adapter.ts
+++ src/app/shared/classes/tc-date-adapter.ts
@@ -1,26 +1,56 @@
 import { NativeDateAdapter } from '@angular/material';
+export class TcDateAdapter extends NativeDateAdapter {
 
-export class TcDateAdapter extends NativeDateAdapter  {
-  
   getFirstDayOfWeek(): number {
     return 1;
   }
   getDayOfWeekNames(): string[] {
-    return ['M', 'D', 'M', 'D', 'F', 'S', 'S'];
+    if(localStorage.getItem('lang') === 'de') {
+      return [ 'Son', 'Mon', 'Die', 'Mit', 'Don', 'Fre', 'Sam'];
+    } else {
+      return super.getDayOfWeekNames('short');
+    }
   }
   getMonthNames(): string[] {
-    return ['JAN', 'FEB', 'MÄR', 'APR', 'KANN', 'JUN', 'JUL', 'AUG', 'SEP', 'OKT', 'DEZ'];
+    if(localStorage.getItem('lang') === 'de') {
+      return ['JAN', 'FEB', 'MÄR', 'APR', 'KANN', 'JUN', 'JUL', 'AUG', 'SEP', 'OKT', 'DEZ'];
+    } else {
+      return super.getMonthNames('short');
+    }
   }
-  // export const MY_DATE_FORMATS = {
-  //   parse: {
-  //     dateInput: 'LL',
-  //   },
-  //   display: {
-  //     dateInput: 'LL',
-  //     monthYearLabel: 'MMM YYYY',
-  //     dateA11yLabel: 'LL',
-  //     monthYearA11yLabel: 'MMMM YYYY',
-  //   },
-  // };
-  
+  format(date: Date, displayFormat: Object): string {
+    if (displayFormat == "input") {
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      let year = date.getFullYear();
+      return year + '.' + this._to2digit(month) + '.' + this._to2digit(day);
+    } else {
+      return date.toDateString();
+    }
+  }
+
+  private _to2digit(n: number) {
+    return ('00' + n).slice(-2);
+  }
 }
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'input',
+  },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'input',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+// export const MY_DATE_FORMATS = {
+//   display: {
+//     // dateInput: { month: 'short', year: 'numeric', day: 'numeric' },
+//     dateInput: 'input',
+//     monthYearLabel: {year: 'numeric', month: 'short'},
+//     dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
+//     monthYearA11yLabel: {year: 'numeric', month: 'long'},
+//   }
+// };
