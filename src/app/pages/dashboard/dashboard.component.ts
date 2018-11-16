f89eb8672d648635e047cc9766d1337a4a45b721
nermeen-mattar <nermeenmattar5d@gmail.com>
2018-11-16 19:35:54 +0200

    Adding example of business info

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -15,7 +15,33 @@ interface CardSettings {
 export class DashboardComponent implements OnDestroy {
 
   private alive = true;
-
+  // example of busienss info (apparently we do have the type :D)
+  businessInfo = 
+    {
+      'id': "3f522ee8-7e69-4d78-aeb5-5278aaf21558",
+      "name": "Burger",
+      "type": 'F&B',
+      'country': 'JO',
+      'currency': 'JOD',
+      'phone': '797405021',
+      'address': 'Amman, Amman,Jordan',
+      'image': {
+        '256': 'https://posrocket-production.s3.amazonaws.com:443/3f522ee87e694d78aeb55278aaf21558/6cf8aee0e66a4f40b91539668e67f613-256.png'
+      },
+      'social': {
+        'id': '5072b345-696d-49c6-9def-e62981be1cde',
+        'facebook': null,
+        'instagram': null,
+        'snapchat': null,
+        'twitter': null
+      },
+      'images': {
+        'receipt_logo': {
+          '256': 'https://posrocket-production.s3.amazonaws.com:443/3f522ee87e694d78aeb55278aaf21558/6cf8aee0e66a4f40b91539668e67f613-256.png'
+        }
+      }
+    };
+    
   lightCard: CardSettings = {
     title: 'Light',
     iconClass: 'nb-lightbulb',
