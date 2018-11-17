cd3fa0b5e920363be75094f9d02a787997da7e2e
Hadeel <hadylgk87@gmail.com>
2018-11-17 18:20:35 +0200

    set up server side

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
 
@@ -9,13 +8,14 @@ import { AnalyticsService } from './@core/utils/analytics.service';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-
-  constructor(private analytics: AnalyticsService, themeService: NbThemeService) {
+  constructor(
+    private analytics: AnalyticsService,
+    themeService: NbThemeService,
+  ) {
     themeService.changeTheme('default');
   }
 
   ngOnInit() {
-    
     this.analytics.trackPageViews();
   }
 }
