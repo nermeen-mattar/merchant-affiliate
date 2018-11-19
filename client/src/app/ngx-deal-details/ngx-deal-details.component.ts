a4918b7e701124ed453b36e47811f56ceae6fe7f
Hadeel <hadylgk87@gmail.com>
2018-11-20 00:08:11 +0200

    display deal details

--- client/src/app/ngx-deal-details/ngx-deal-details.component.ts
+++ client/src/app/ngx-deal-details/ngx-deal-details.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// Services
+import { DealApi } from '../../sdk';
 
 @Component({
   selector: 'ngx-deal-details',
   templateUrl: './ngx-deal-details.component.html',
-  styleUrls: ['./ngx-deal-details.component.scss']
+  styleUrls: ['./ngx-deal-details.component.scss'],
 })
 export class NgxDealDetailsComponent implements OnInit {
+  public dealInfo;
+  public dealLimit: any;
+  private dealId: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private dealApi: DealApi) {
+    this.route.url.subscribe(values => {
+      this.dealId = values[1].path;
+    });
   }
 
+  ngOnInit() {
+    if (this.dealId) {
+      this.dealApi.findById(this.dealId).subscribe(data => {
+        this.dealInfo = data;
+        if (data['target_businesses'] !== null) {
+          this.dealLimit =
+            Number(data['limit']) - data['target_businesses'].length;
+        } else {
+          this.dealLimit = data['limit'];
+        }
+      });
+    }
+  }
 }
